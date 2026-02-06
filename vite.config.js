2a5e963adfdc800938c42e32f926f9ad576be170
Jerry <127914567+JerryYang3444251@users.noreply.github.com>
2026-02-06 07:55:52 +0000

    Update: allowedHosts set to 'all' for both server and preview

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: [".csb.app"]
+    allowedHosts: ['all']
   },
   preview: {
-    allowedHosts: [".csb.app"]
+    allowedHosts: ['all']
   }
 });
