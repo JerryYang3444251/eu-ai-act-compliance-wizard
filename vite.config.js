ec5d0dcc2d3e6942d5b44688a9ed1d677a383ac3
Jerry Yang <ZZ0183858@example.com>
2026-02-06 14:28:56 +0800

    Allow all hosts in Vite config for CodeSandbox

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: [
-      "gfqljr-5173.csb.app",
-      "*.csb.app",
-      "localhost",
-      "127.0.0.1"
-    ]
+    allowedHosts: "all"
   }
 });
